a169715085199abf20b7a5e7b9292b9b3bbafc94
Trip Volpe <mraccident@gmail.com>
2011-05-06 00:09:48 -0700

    Merged testmain and main: unit tests will be built in to the main executable from now on.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <string>
+
+#include <gtest/gtest.h>
+
 
 int main(int argc, char** argv)
 {
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string s(argv[i]);
+
+        if (s == "--test")
+        {
+            ::testing::InitGoogleTest(&argc, argv);
+            return RUN_ALL_TESTS();
+        }
+    }
+
+    std::cout << "Nothing done!\n";
     return 0;
+}
+
+
+//
+// A dummy unit test
+//
+TEST(maintestcase, test1)
+{
+    ASSERT_TRUE(true);
 }
